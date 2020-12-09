4af4f81397db03f5f8234db836b84842e7fa83c0
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-12-09 20:38:18 +0100

    Created bin/cicleci.py

---
+++ bin/cicleci.py
@@ -0,0 +1,19 @@
+"""cicleci
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-12-9"
+
+class cicleci:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(cicleci.cicleci())
+	
+	def cicleci():
+		"""cicleci first funtion
+		"""
+		return "cicleci" #for the test (see test_cicleci.py)
+		
+if __name__ == "__main__":
+	cicleci()
